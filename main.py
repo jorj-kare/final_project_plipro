081443d247c18a364d7a03cf085a8c0aee61b7a3
jorj-kare <jorj.kare@gmail.com>
2024-04-20 14:09:38 +0200

    create main window

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from tkinter import ttk
 import matplotlib as mpl
 
 
+# -------- Global variables ------------
+
+color_main_window = "#F5E1FD"
+
+
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        # ---------------
+        self.geometry("1200x800")
+        self.resizable(0, 0)
+        self.title("Simple Evolutionary Strategies")
+        self.config(background=color_main_window)
+
+
 if __name__ == "__main__":
 
-    print("Hello Word")
+    app = App()
+    app.mainloop()
